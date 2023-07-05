aeb9c6affec51369c7e69f4059857a4794e5ded8
Pedro Lara <brownsgameplays14@gmail.com>
2023-07-04 21:19:15 -0500

    updated the cv pdf file

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/CURRICULUM-VITAE-PEDRO-LARA.pdf"
+                  href="/CV-Pedro-Lara-Web-Developer.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}
